c70e4aa56263fbaa622c5f034596da55d009f209
joako <joako@Joako>
2026-06-23 22:32:49 -0500

    Mejoras en libros y base de controlador

--- Libreria_UTP/src/libreria/controlador/PrestamoController.java
+++ Libreria_UTP/src/libreria/controlador/PrestamoController.java
@@ -1,5 +1,17 @@
 package libreria.controlador;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PrestamoController {
+public class PrestamoController implements ActionListener {
+    private Object vista;
     
+    public PrestamoController(Object vista) {
+        this.vista = vista;
+    }
+    public void iniciar() {
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 }
